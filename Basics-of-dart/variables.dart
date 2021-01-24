ff4f3d37835f4b312557b09c6fa2a1a1f1620b90
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-24 05:40:28 -0800

    Create variables.dart

---
+++ Basics-of-dart/variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 2;
+  String str = 'abc';
+  double d = 12.34;
+  num n = 12345;
+  bool val = true;
+  print('value of a is $a');
+  print('value of str is $str');
+  print('value of d is $d');
+  print('value of n is $n');
+  print(val);
+}
